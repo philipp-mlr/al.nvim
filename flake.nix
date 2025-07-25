79ef9441fcb6cd50fe06cb9b562c4e148492b0f7
Philipp Müller <hello@philipp.software>
2025-07-25 21:17:05 +0200

    feat: add nix flake config

--- flake.nix
+++ flake.nix
@@ -9,35 +9,26 @@
     self,
     nixpkgs,
   }: let
-    systems = ["x86_64-linux" "aarch64-linux"]; # optionally extend for other platforms
-    eachSystem = nixpkgs.lib.genAttrs systems (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in {
-      packages.al-nvim = pkgs.vimUtils.buildVimPlugin {
-        pname = "al-nvim";
-        version = "unstable";
-
-        src = ./.;
-
-        dependencies = with pkgs.vimPlugins; [
-          nui-nvim
-          nvim-dap
-          nvim-dap-ui
-          nvim-nio
-          nvim-dap-virtual-text
-        ];
-
-        meta = with pkgs.lib; {
-          description = "A Neovim plugin for AL language support in Microsoft Dynamics 365 Business Central development.";
-          homepage = "https://github.com/abonckus/al.nvim";
-          license = licenses.mit;
-          maintainers = with maintainers; [abonckus];
-        };
+    pkgs = import nixpkgs {system = builtins.currentSystem;};
+  in {
+    packages.al-nvim = pkgs.vimUtils.buildVimPlugin {
+      pname = "al-nvim";
+      version = "unstable";
+      src = ./.;
+      dependencies = with pkgs.vimPlugins; [
+        nui-nvim
+        nvim-dap
+        nvim-nio
+        nvim-dap-virtual-text
+      ];
+      meta = with pkgs.lib; {
+        description = "A Neovim plugin for AL language support in Microsoft Dynamics 365 Business Central development.";
+        homepage = "https://github.com/philipp-mlr/al.nvim";
+        license = licenses.mit;
+        maintainers = with maintainers; [abonckus];
       };
+    };
 
-      # Optional: defaultPackage and defaultApp
-      defaultPackage = eachSystem.${system}.packages.al-nvim;
-    });
-  in
-    eachSystem;
+    defaultPackage = self.packages.al-nvim;
+  };
 }
---
+++ lua/al/luasnippets/placeholder.lua
@@ -0,0 +1,4 @@
+-- Placeholder snippet file
+
+-- This file exists to prevent build failures in case no other snippets are present in the luasnippets directory.
+
