d1cbdb072c1bd2e5532ee956afd2466e75acfdf2
Philipp Müller <hello@philipp.software>
2025-07-25 21:40:42 +0200

    feat: add nix flake config

--- flake.nix
+++ flake.nix
@@ -3,33 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
   };
 
-  outputs = {
+  outputs = inputs @ {
     self,
-    nixpkgs,
-  }: let
-    pkgs = import nixpkgs {system = builtins.currentSystem;};
-  in {
-    packages.al-nvim = pkgs.vimUtils.buildVimPlugin {
-      pname = "al-nvim";
-      version = "unstable";
-      src = ./.;
-      dependencies = with pkgs.vimPlugins; [
-        nui-nvim
-        nvim-dap
-        nvim-nio
-        nvim-dap-virtual-text
-        noice-nvim
-      ];
-      meta = with pkgs.lib; {
-        description = "A Neovim plugin for AL language support in Microsoft Dynamics 365 Business Central development.";
-        homepage = "https://github.com/philipp-mlr/al.nvim";
-        license = licenses.mit;
-        maintainers = with maintainers; [abonckus];
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+
+      perSystem = {pkgs, ...}: {
+        packages.default = pkgs.vimUtils.buildVimPlugin {
+          pname = "al-nvim";
+          version = "unstable";
+          src = self;
+          dependencies = with pkgs.vimPlugins; [
+            nui-nvim
+            nvim-dap
+            nvim-nio
+            nvim-dap-virtual-text
+            noice-nvim
+          ];
+        };
       };
     };
-
-    defaultPackage = self.packages.al-nvim;
-  };
 }
